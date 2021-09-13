f8a0586abec2720f02cbb4d6e05f92a69c3a7356
GoutamrajSahu <sahugoutamraj@gmail.com>
2021-09-14 03:23:53 +0530

    Add passport authentication

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const cors = require('cors'); // to avoid all core problems
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -12,11 +15,30 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-
-
 app.use(cors());
 
+app.use(session({
+  secret:"Secret will be secret when we don't expose it.",
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/INSTA_DEF_DB",{useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // const todoSchema = mongoose.Schema({
 //    title:String,
@@ -29,10 +51,34 @@ app.route("/")
 .get((req, res)=>{
    res.send("Hello");
 })
+
+app.route("/signup")
 .post((req, res)=>{
-   console.log(req.body);
-   res.send(req.body);
+   
+  User.register({username: req.body.email}, req.body.password, (err,user)=>{
+   if(err){
+     res.send(false);
+   }else{
+     res.send(true);
+   }
+  });
+  //  console.log("signUpData: ");
+  //  console.log(req.body);
+  //  res.send(req.body);
 });
+
+app.route("/login")
+.post((req, res)=>{
+
+  // console.log("loginData: ")
+  //  console.log(req.body);
+  //  res.send(req.body);
+});
+
+
+
+
+
 
 
 app.listen(5000, function() {
